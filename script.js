60f2c87ace632268c3408234c31bd3db6bdbc940
NgocMinhThuNguyen <94945964+NgocMinhThuNguyen@users.noreply.github.com>
2024-03-06 14:57:27 +0800

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const projects = document.querySelectorAll(".project-item");
+
+let project = document.querySelector("#web-app-1");
+
+project.addEventListener("click", (event) => {
+  project.style.display = "none";
+})
